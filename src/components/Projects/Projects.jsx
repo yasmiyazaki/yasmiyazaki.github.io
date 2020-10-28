f29775e910ddb3cc167dbe410d5a74018526576e
yasmiyazaki <kizayami.yasu@gmail.com>
2020-10-28 21:50:24 +0900

    init

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -21,7 +21,7 @@ const Projects = () => {
       setIsDesktop(false);
     }
   }, []);
-
+  console.log(projects);
   return (
     <section id="projects">
       <Container>
@@ -55,7 +55,7 @@ const Projects = () => {
                         className="cta-btn cta-btn--hero"
                         href={url || '#!'}
                       >
-                        See Live
+                        Detail
                       </a>
 
                       {repo && (
--- src/mock/data.js
+++ src/mock/data.js
@@ -2,25 +2,28 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: 'Yasu', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: "Hi, I'm",
+  name: 'Yasu',
+  subtitle: 'I deliver meaningful customer experience.',
+  cta: 'More',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  paragraphOne:
+    'For the past seven years, I have been transforming routine tasks into meaningful activities.',
+  paragraphTwo:
+    'I started my career in business process reengineering and enterprise system implementation. Now, I am a software engineer building online travel services that international tourists can explore the beauty of Japan',
+  paragraphThree:
+    'What is common is I eliminate inefficiency and help users focus what matters the most.',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -28,30 +31,30 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'kaimono.png',
+    title: 'Online Tex Free Shopping',
+    info: 'UX Research, UX Design',
     info2: '',
-    url: '',
+    url: 'kaimono',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'foodget.png',
+    title: 'Grocery Shopping',
+    info: 'UX Research, UX Design, Prototyping, Web Development',
+    info2: '',
+    url: 'foodget',
     repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'boogiewoogie.png',
+    title: 'Motion Tracking Dance Practice App',
+    info: 'Web Development',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/crazy-bananas/boogie-woogie', // if no repo, the button will not show up
   },
 ];
 
@@ -67,28 +70,13 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/yasuhito-miyazaki/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/yasmiyazaki/',
     },
   ],
-};
-
-// Github start/fork buttons
-export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
 };
